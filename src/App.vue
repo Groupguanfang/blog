1fb121a964ea0175fce51edb297883a65deb1af5
Naily <zero@naily.cc>
2024-07-22 16:12:18 +0800

    feat: theme-color

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
-<script setup lang="tsx"></script>
+<script setup lang="tsx">
+useHead({
+  meta: [
+    { name: 'theme-color', content: computed(() => isDark.value ? '#000000' : '#ffffff') },
+  ],
+})
+</script>
 
 <template>
   <Suspense>
